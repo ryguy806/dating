6b071d33eb62067ffe12258f4b33508bd01d2ce2
Ryan Guelzo <rguelzo@mail.greenriver.edu>
2019-04-08 17:15:48 -0400

    updated the index page

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+//error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once('vendor/autoload.php');
+
+$f3 = Base::instance();
+
+$f3->route('GET /', function (){
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+$f3->run();
+?>
