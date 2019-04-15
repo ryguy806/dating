7e6463faf76597b66a56b5e7438e4b6662669d16
Ryan Guelzo <rguelzo@mail.greenriver.edu>
2019-04-15 17:48:47 -0400

    Created personal info form, updated index.php

--- index.php
+++ index.php
@@ -12,6 +12,9 @@ error_reporting(E_ALL);
 //require the autoload
 require_once('vendor/autoload.php');
 
+//Includes the header form
+include('views/header.html');
+
 //Creates the instance of the base class
 $f3 = Base::instance();
 
@@ -19,6 +22,10 @@ $f3 = Base::instance();
 $f3->route('GET /', function (){
     $view = new Template();
     echo $view->render('views/home.html');
+});
+$f3->route('GET /', function (){
+   $view = new Template();
+   $view->render('views/personalinformation.html');
 });
 
 //Run fat-free
