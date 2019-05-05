546cf56fff9b928e6e8a943ef184d0460a9767bf
Ryan Guelzo <rguelzo@mail.greenriver.edu>
2019-05-04 21:05:46 -0400

    added validation and made forms sticky

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ require_once('vendor/autoload.php');
 //Creates the instance of the base class
 $f3 = Base::instance();
 
+
 //Specified the default route
 $f3->route('GET|POST /', function (){
 
@@ -33,33 +34,105 @@ $f3->route('GET|POST /profile-start', function (){
    echo $view->render('views/personalinformation.html');
 });
 
-$f3->route('GET|POST /profile-continue', function (){
+$f3->route('GET|POST /profile-continue', function ($f3){
 
-    $_SESSION['firstname'] = $_POST['firstname'];
-    $_SESSION['lastname'] = $_POST['lastname'];
-    $_SESSION['age'] = $_POST['age'];
-    $_SESSION['gender'] = $_POST['gender'];
-    $_SESSION['phone'] = $_POST['phone'];
+    if(!empty($_POST)) {
 
+        //get the form data
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $age = $_POST['age'];
+        $gender = $_POST['gender'];
+        $phone = $_POST['phone'];
+
+        //add to the hive
+        $f3->set('firstname', $firstname);
+        $f3->set('lastname', $lastname);
+        $f3->set('age', $age);
+        $f3->set('gender', $gender);
+        $f3->set('phone', $phone);
+
+        if(validForm1()){
+            $_SESSION['firstname'] = $firstname;
+            $_SESSION['lastname'] = $lastname;
+            $_SESSION['age'] = $age;
+            if(!empty($gender)){
+                $_SESSION['gender'] = $gender;
+            }
+            else{
+                $_SESSION['gender'] = 'No gender selected.';
+            }
+            $_SESSION['phone'] = $phone;
+
+            $f3->reroute('/profile-interests');
+        }
+    }
     $view = new Template();
     echo $view->render('views/profileEntry.html');
 });
 
-$f3->route('GET|POST /profile-interests', function (){
+$f3->route('GET|POST /profile-interests', function ($f3){
 
-    $_SESSION['email'] = $_POST['email'];
-    $_SESSION['state'] = $_POST['state'];
-    $_SESSION['seeking'] = $_POST['seeking'];
-    $_SESSION['bio'] = $_POST['bio'];
+    $email = $_POST['email'];
+    $state = $_POST['state'];
+    $seeking = $_POST['seeking'];
+    $bio = $_POST['bio'];
+
+    $f3->set('email', $email);
+    $f3->set('state', $state);
+    $f3->set('seeking', $seeking);
+    $f3->set('bio', $bio);
+
+    if(validEmail()){
+        $_SESSION['email'] = $_POST['email'];
+        if(!empty($state)){
+            $_SESSION['state'] = $state;
+        }
+        else{
+            $_SESSION['state'] = 'No state selected.';
+        }
+        if(!empty($seeking)){
+            $_SESSION['seeking'] = $seeking;
+        }
+        else{
+            $_SESSION['seeking'] = 'No input.';
+        }if(!empty($bio)){
+            $_SESSION['bio'] = $bio;
+        }
+        else{
+            $_SESSION['bio'] = 'No bio entered.';
+        }
+    }
+
 
     $view = new Template();
     echo $view->render('views/interests.html');
 });
 
-$f3->route('GET|POST /summary', function (){
+$f3->route('GET|POST /summary', function ($f3){
+    if(!empty($_POST)) {
+        $outdoor = $_POST['outdoor'];
+        $indoor = $_POST['indoor'];
 
-    $_SESSION['indoor'] = implode(', ', $_POST['indoor']);
-    $_SESSION['outdoor'] = implode(', ', $_POST['outdoor']);
+        $f3->set('outdoor', $outdoor);
+        $f3->set('indoor', $indoor);
+
+        if (validForm3()) {
+            if(!empty($outdoor)){
+                $_SESSION['outdoor'] = implode(', ', $outdoor);
+            }
+            else{
+                $_SESSION['outdoor'] = 'No outdoor selections made.';
+            }
+            if(!empty($outdoor)){
+                $_SESSION['indoor'] = implode(', ', $indoor);
+            }
+            else{
+                $_SESSION['indoor'] = 'No indoor selections made.';
+            }
+        }
+
+    }
 
 
     $view = new Template();
---
+++ model/validation.php
@@ -0,0 +1,77 @@
+<?php
+
+
+function validForm1()
+{
+    global $f3;
+    $isValid = true;
+    if (!validname($f3->get('firstname'), $f3->get('lastname'))) {
+        $isValid = false;
+        $f3->set("errors['name']", "Not a valid name, please enter a valid name.");
+    }
+    if (!validAge($f3->get('age'))) {
+        $isValid = false;
+        $f3->set("errors['age']", "Please enter 1 or more.");
+    }
+    if (!validPhone($f3->get('phone'))) {
+        $isValid = false;
+        $f3->set("errors['phone']", "Please enter a valid phone number (123-456-7890).");
+    }
+    return $isValid;
+}
+
+function validForm3()
+{
+    global $f3;
+    $isValid = true;
+    if (!validOutdoor($f3->get('outdoor'))) {
+        $isValid = false;
+        $f3->set("errors['outdoor']", "");
+    }
+    if (!validIndoor($f3->get('age'))) {
+        $isValid = false;
+        $f3->set("errors['age']", "Please enter 1 or more.");
+    }
+    return $isValid;
+}
+
+function validName($fname, $lname){
+    return (!empty($fname) && ctype_alpha($fname)) && (!empty($lname) && ctype_alpha($lname));
+}
+
+function validAge($age){
+    return !empty($age) && ctype_digit($age);
+}
+
+function validPhone($phone){
+    return preg_match("/^[0-9]{3}-[0-9]{4}-[0-9]{4}$/", $phone);
+}
+
+function validEmail($email){
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+function validOutdoor($outdoor){
+
+    $options = array('spellcasting', 'horse-riding', 'hiding', 'looting', 'fighting', 'healing');
+    $verified = array();
+    foreach ($options as $option) {
+        if(isset($outdoor)){
+            array_push($verified, $option);
+        }
+    }
+    return sizeof($verified) > 0;
+}
+
+function validIndoor($indoor){
+
+    $options = array('trading', 'dragon-chess', 'drinking', 'reading', 'pickpocketing', 'lock-picking',
+        'story-telling', 'smithing');
+    $verified = array();
+    foreach ($options as $option) {
+        if(isset($indoor)){
+            array_push($verified, $option);
+        }
+    }
+    return sizeof($verified) > 0;
+}
